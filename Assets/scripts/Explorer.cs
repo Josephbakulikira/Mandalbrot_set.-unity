01a37b72f0f8231bde0c9a9dd30874b5c18b394a
joseph <auctux@gmail.com>
2019-12-23 18:20:15 +0530

    first commit

---
+++ Assets/scripts/Explorer.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explorer : MonoBehaviour
+{
+    private float smoothA;
+    private Vector2 smoothP;
+    private float smoothS;
+
+    [SerializeField]
+    private Material material;
+    [SerializeField]
+    private Vector2 p; // position
+    [SerializeField]
+    private float s; //scale of the mandalbrot
+    [SerializeField]
+    private float a;
+    
+    
+
+    void FixedUpdate()
+    {
+        UpdateShafer();
+        handleInput();
+    }
+    void handleInput()
+    {
+        //scale
+        if (Input.GetKey(KeyCode.KeypadPlus) || Input.GetKey(KeyCode.E))
+            s *= 0.99f;
+        if (Input.GetKey(KeyCode.KeypadMinus) || Input.GetKey(KeyCode.Q))
+            s *= 1.01f;
+        //angle
+        if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.Keypad4))
+            a -= 0.01f;
+        if (Input.GetKey(KeyCode.C) || Input.GetKey(KeyCode.Keypad6))
+            a += 0.01f;
+        //directtions
+
+        Vector2 direction = new Vector2(0.01f * s, 0);
+        float si = Mathf.Sin(a);
+        float co = Mathf.Cos(a);
+        direction = new Vector2(direction.x * co - direction.y * si, direction.x * si + direction.y * co);
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            p -= direction;
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            p += direction;
+        direction = new Vector2(-direction.y, direction.x);
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            p += direction;
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            p -= direction;
+        
+    }
+    private void UpdateShafer() {
+        smoothP = Vector2.Lerp(smoothP, p, 0.04f);
+        smoothS = Mathf.Lerp(smoothS, s, 0.04f);
+        smoothA = Mathf.Lerp(smoothA, a, 0.04f);
+        float aspect = (float)Screen.width / (float)Screen.height;
+        float sx = smoothS; // scale x
+        float sy = smoothS; // scale y
+
+        if (aspect > 1f)
+        {
+            sy /= aspect;
+        }
+        else
+        {
+            sx *= aspect;
+        }
+
+        material.SetVector("_Surface", new Vector4(smoothP.x, smoothP.y, sx, sy));
+        material.SetFloat("_Angle", smoothA);
+    }
+}
---
+++ Assets/shaders/fractor.shader
@@ -0,0 +1,78 @@
+﻿Shader "Explorer/fractor"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+		_Surface("Surface", vector) = (0, 0, 4, 4)
+			_Angle("Angle", range(-3.14159265358, 3.14159265358)) = 0
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+			float4 _Surface;
+			float _Angle;
+            sampler2D _MainTex;
+
+			float2 rot(float2 p, float2 pivot, float a)
+			{
+				float s = sin(a);
+				float c = cos(a);
+				p -= pivot;
+				p = float2(p.x* c-p.y*s, p.x*s+p.y*c);
+				p += pivot;
+
+				return p;
+			}
+            fixed4 frag (v2f i) : SV_Target
+            {
+                float2 c = _Surface.xy + (i.uv - 0.5)* _Surface.zw;
+
+				c = rot(c, _Surface.xy, _Angle);
+
+				float2 z;
+				float iterations;
+				for (iterations = 0; iterations<400; iterations++){
+					z = float2(z.x*z.x-z.y*z.y,2*z.x*z.y) + c;
+					if(length(z) > 2)
+					{
+						break;
+					}
+				}
+				float mandalbrot = sqrt(iterations / 400);
+				float4 col = sin(float4(.6,.90,.2, 1) * mandalbrot);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
